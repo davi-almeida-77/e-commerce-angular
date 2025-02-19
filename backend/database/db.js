2ff48dc46bbc58b1696cf6022272323b1287c226
DaviAlmeidaD3V <DaviAlmeidaDEV@hotmail.com>
2025-02-19 13:02:30 -0300

    [UPDATE] Environment variables

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,14 +1,11 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root', 
-  password: '58915281', 
-  database: 'ecommerce_db',
-  // host: process.env.DB_HOST,
-  // user: process.env.DB_USER, 
-  // password: process.env.DB_PASSWORD, 
-  // database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER, 
+  password: process.env.DB_PASSWORD, 
+  database: process.env.DB_NAME,
 });
 
 connection.connect((err) => {
