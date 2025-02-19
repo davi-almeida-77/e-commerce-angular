f847381bf8e522228c782fbf6b7dc8a28a80fc68
DaviAlmeidaD3V <DaviAlmeidaDEV@hotmail.com>
2025-02-19 13:04:53 -0300

    [UPDATE] Adding Auth Route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,17 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-
-
 const usersRoute = require("./users");
 const productsRoute = require("./products");
+const authRoute =  require("./auth")
 
-
-
-router.use("/api/v1/users", usersRoute);  
-router.use("/api/v1/products", productsRoute); 
-
-
+router.use("/api/auth", authRoute)
+router.use("/api/users", usersRoute);  
+router.use("/api/products", productsRoute); 
 
 module.exports = router;
-
