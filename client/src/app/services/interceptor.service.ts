1b26096f138c7a037cc988f544f6fbe1b953ece9
DaviAlmeidaD3V <DaviAlmeidaDEV@hotmail.com>
2025-02-22 00:20:43 -0300

    [UPDATE] Add Token Configuration, Managed By In-Session Storage

---
+++ client/src/app/services/interceptor.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpHandler,  HttpRequest, HttpResponse, 
+HTTP_INTERCEPTORS, 
+HttpEvent} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { TokenStorageService } from './token-storage.service';
+
+
+
+const TOKEN_HEADER_KEY = 'x-access-token';
+
+@Injectable()
+export class MyInterceptor implements HttpInterceptor {
+
+  constructor( private _token: TokenStorageService  ) { }
+
+intercept(
+  req: HttpRequest<any>, 
+  next: HttpHandler
+): Observable<HttpEvent<any>> {
+  let authenticationToken = req;
+  let sessionToken = this._token.getToken();
+  if (sessionToken != null) {
+    authenticationToken = req.clone({
+      headers: req.headers.set(TOKEN_HEADER_KEY, sessionToken),
+    });
+  }
+  return next.handle(authenticationToken);
+  }
+}
+
+export const authInterceptorProviders = [
+  { provide: HTTP_INTERCEPTORS, useClass: MyInterceptor, multi: true },
+];
