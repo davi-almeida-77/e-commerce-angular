7cd83eb2f36fd56a3486b0c56ed90bd689f58ed3
DaviAlmeidaD3V <DaviAlmeidaDEV@hotmail.com>
2025-02-26 09:43:48 -0300

    [UPDATE] Add Register and Login, With express Authentication

--- client/src/app/components/auth/login/login.component.ts
+++ client/src/app/components/auth/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';  // Adicione essa linha
+
+
+
 
 @Component({
   selector: 'app-login',
@@ -7,5 +13,41 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  email: string = '';
+  u_password: string = '';
+  loading: Boolean = false;
+  error:any = '';
+
+  constructor( private _auth: AuthService, private _router: Router ) {}
+
+ngOnInit(): void {}
+
+onSubmit () {
+  this.loading = true;
+  this.error = '';
+  if (!this.email || !this.u_password) {
+    console.log("Fill out all Forms, and Try Again ")
+    return; 
+  }
+  else {
+    this._auth.login({ email: this.email, password: this.u_password })
+    .subscribe(
+      response => {
+        this.loading = false;
+        this._router.navigate(['/']);
+      },
+      ( err ) => {
+        console.log( err )
+        this.error = err.error.message;
+        this.loading = false;
+      }
+    )
+  };
+}
+
+canSubmit(): boolean {
+  return this.email.length > 0 && this.u_password.length > 0;
+}
 
 }
--- client/src/app/components/auth/register/register.component.ts
+++ client/src/app/components/auth/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
@@ -8,4 +12,50 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  username:  string = '';
+  f_name: string = '';
+  l_name: string = '';
+  email: string = '';
+  u_password: string = '';
+  m_password: string = '';
+  loading: Boolean = false;
+  error: any = '';
+
+  constructor ( private _auth: AuthService, private _router: Router ) {}
+
+  ngOnInit(): void {}
+
+
+matchPasswords( u_password: string, m_password: string ): boolean {
+  return u_password === m_password;
+}
+
+  onSubmit() {
+    this.loading = true;
+    this.error = '';
+    if (!this.username || !this.f_name || !this.l_name || !this.email || !this.u_password) {
+      console.log("Please fill out all Forms, and Try again ")
+      return;
+    }
+    if (!this.matchPasswords(this.u_password, this.m_password)) {
+      console.log("The passwords do not match. Please try again.");
+      return;
+    }
+    else {
+      this._auth.register({ username: this.username, f_name: this.f_name, l_name: this.l_name, email: this.email, u_password: this.u_password }).
+      subscribe(
+        response => {
+          this.loading = false;
+          this._router.navigate(['/']);
+        },
+        (err) => {
+          console.log(err)
+        }
+      )
+    }
+  }
+
+  canSubmit(): boolean {
+    return this.email.length > 0 && this.u_password.length > 0;
+  }
 }
