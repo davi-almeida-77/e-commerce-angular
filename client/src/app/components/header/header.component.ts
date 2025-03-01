3e96bafe38217d6ab4d29c098b8d44c6487897d3
DaviAlmeidaD3V <DaviAlmeidaDEV@hotmail.com>
2025-03-01 00:59:48 -0300

    [UPDATE] Add CartItems Status on Header

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { CartService } from '../../services/cart.service';
+import { Subscription } from 'rxjs';  
 
 @Component({
   selector: 'app-header',
-  standalone: false,
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css'],
+  standalone: false,
 })
-export class HeaderComponent {
+export class HeaderComponent  {
+  totalItens: number = 0;  
+  cartSubscription!: Subscription; 
 
+  constructor(private cartService: CartService) {}
+
+
+  ngOnInit() {
+    this.cartService.totalItens$.subscribe(total => {
+      this.totalItens = total; 
+    });
+  }
 }
