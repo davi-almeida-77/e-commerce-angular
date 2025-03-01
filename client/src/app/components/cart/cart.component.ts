a639146d6e6428cbab704982d4db82e6f4ff5e05
DaviAlmeidaD3V <DaviAlmeidaDEV@hotmail.com>
2025-03-01 00:54:53 -0300

    [UPDATE] Cart Component, Updated, With Data, and Orders

--- client/src/app/components/cart/cart.component.ts
+++ client/src/app/components/cart/cart.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../services/cart.service';
+import { productModel } from '../../shared/models/product.model';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-cart',
-  standalone: false,
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.css'
+  styleUrls: ['./cart.component.css'],
+  imports: [CommonModule, RouterModule],
+  standalone: true,
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  cartItems: productModel[] = [];  
+  totalItems: number = 0;           
+  totalPrice: number = 0;          
 
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    
+    this.cartService.getCartItems().subscribe((cart: productModel[]) => {
+      this.cartItems = cart; 
+      this.totalItems = this.cartService.getTotalItems(); 
+      this.totalPrice = this.cartService.getTotalPrice(); 
+    });
+  }
 }
