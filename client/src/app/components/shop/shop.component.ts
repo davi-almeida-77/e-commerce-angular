41c39cf0837be6114d970197568a5c1836cc377f
DaviAlmeidaD3V <DaviAlmeidaDEV@hotmail.com>
2025-03-03 18:59:52 -0300

    [UPDATE] Functions off Add Product, and Dynamic Route to Single Product

--- client/src/app/components/shop/shop.component.ts
+++ client/src/app/components/shop/shop.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { CartService } from '../../services/cart.service';
+
+import { productModel } from '../../shared/models/product.model'; 
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-shop',
   standalone: false,
   templateUrl: './shop.component.html',
-  styleUrl: './shop.component.css',
+  styleUrls: ['./shop.component.css'], 
 })
-export class ShopComponent {
+export class ShopComponent implements OnInit {
+  products: productModel[] = []; 
 
+  constructor(private productService: ProductService, private router: Router ,  private cartService: CartService ,  ) {}  
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe(
+      (data: productModel[]) => {  
+        this.products = data; 
+      },
+      (error) => {
+        console.error('Erro ao buscar produtos:', error);
+      }
+    );
+  }
+
+  SingleProductPage(productId: number): void {
+    this.router.navigate([`/product/${productId}`]);
+  }
+
+  addToCart(product: productModel): void {
+    this.cartService.addToCart({
+      ...product,
+      quantity: 1,  
+    });
+  }
+  
 }
