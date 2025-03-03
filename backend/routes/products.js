e1040710efbbeff93fd68f3bfadd0cf458f057b9
DaviAlmeidaD3V <DaviAlmeidaDEV@hotmail.com>
2025-03-03 18:32:59 -0300

    [UPDATE] Get Single Product

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -13,5 +13,23 @@ router.get("/", (req, res) => {
     });
   });
 
-  module.exports = router;
+  router.get("/:id", (req, res) => {
+    const productId = req.params.id; 
 
+    console.log(`Requisição para obter o produto com ID: ${productId}`);
+
+    db.query("SELECT * FROM products WHERE id_product = ?", [productId], (err, results) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ error: 'Erro ao buscar o produto' });
+        } else {
+            if (results.length > 0) {
+                res.json(results[0]);  
+            } else {
+                res.status(404).json({ error: 'Produto não encontrado' });
+            }
+        }
+    });
+});
+
+  module.exports = router;
