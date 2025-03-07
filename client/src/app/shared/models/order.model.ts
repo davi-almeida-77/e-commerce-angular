5dcfa4b915dc78b6cd2ef8c40cdd9549f4b22605
DaviAlmeidaD3V <DaviAlmeidaDEV@hotmail.com>
2025-03-07 15:01:08 -0300

    [ADD] Add order template to get detailed order information

---
+++ client/src/app/shared/models/order.model.ts
@@ -0,0 +1,13 @@
+export interface orderModel {
+    username: string,
+    f_name: string,
+    l_name: string,
+    email: string,
+    order_number: string,
+    order_created_at: string,
+    product_name: string, 
+    product_quantity: number,
+    product_price: number,
+    total_product_value: number
+}
+  
