ed5e76d5b2b61d03de044317c0f17b855f91d179
DaviAlmeidaD3V <DaviAlmeidaDEV@hotmail.com>
2025-03-10 09:21:37 -0300

    [UPDDATE] Get Product Images By ID

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -32,4 +32,28 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/images/:id", (req, res) => {
+  const productId = req.params.id;  
+
+  console.log(`Request to obtain images for product with ID: ${productId}`);
+
+  db.query(
+      "SELECT pi.product_id, pi.image_url, pi.image_type FROM product_images pi JOIN products p ON pi.product_id = p.id_product WHERE pi.product_id = ?", 
+      [productId], 
+      (err, results) => {
+          if (err) {
+              console.log(err);
+              res.status(500).json({ error: 'Error In Get Product Images' });
+          } else {
+              if (results.length > 0) {
+                  res.json(results);  
+              } else {
+                  res.status(404).json({ error: 'No Images Found For Product' });
+              }
+          }
+      }
+  );
+});
+
+
   module.exports = router;
