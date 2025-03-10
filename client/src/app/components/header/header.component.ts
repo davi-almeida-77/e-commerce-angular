bbf69c0856b3f82be5cbaf35184176d07e78a004
DaviAlmeidaD3V <DaviAlmeidaDEV@hotmail.com>
2025-03-10 10:47:35 -0300

    [UPDATE] User State Header Login / Logout Bar

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, OnDestroy } from '@angular/core';
 import { CartService } from '../../services/cart.service';
-import { Subscription } from 'rxjs';  
+import { Subscription } from 'rxjs';
+import { UserService } from '../../services/user.service';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,16 +11,43 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./header.component.css'],
   standalone: false,
 })
-export class HeaderComponent  {
+export class HeaderComponent implements OnInit, OnDestroy {
   totalItens: number = 0;  
-  cartSubscription!: Subscription; 
+  cartSubscription!: Subscription;
+  private authSubscription!: Subscription; 
+  userOn: boolean = false; 
 
-  constructor(private cartService: CartService) {}
+  constructor(
+    private cartService: CartService, 
+    private userService: UserService,
+    private authService: AuthService,
+    private cdr: ChangeDetectorRef,
+    private router: Router
+  ) {}
 
-
-  ngOnInit() {
-    this.cartService.totalItens$.subscribe(total => {
+  ngOnInit(): void {   
+    this.cartSubscription = this.cartService.totalItens$.subscribe(total => {
       this.totalItens = total; 
     });
+
+    this.authSubscription = this.userService.loggedIn$.subscribe(loggedIn => {
+      this.userOn = loggedIn;
+      this.cdr.detectChanges(); 
+      
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.cartSubscription) {
+      this.cartSubscription.unsubscribe();
+    }
+    if (this.authSubscription) {
+      this.authSubscription.unsubscribe();
+    }
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/']);
   }
 }
