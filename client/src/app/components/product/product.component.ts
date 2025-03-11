09d110dd948b4a961667f41a699f1639b8bbbb17
DaviAlmeidaD3V <DaviAlmeidaDEV@hotmail.com>
2025-03-11 15:51:38 -0300

    [UPDATE] Improving HTML And CSS And Add BoxImgs for Product

--- client/src/app/components/product/product.component.ts
+++ client/src/app/components/product/product.component.ts
@@ -2,25 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'; 
 import { ProductService } from '../../services/product.service'; 
 import { CartService } from '../../services/cart.service';
-import { FormsModule } from '@angular/forms';
+import { ProductImage } from '../../shared/models/product.images';
+import { NotificationService } from '../../services/notification.service';
 
 @Component({
   selector: 'app-product',
   standalone: false,
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css'],
-  // imports: [FormsModule]
 })
 export class ProductComponent implements OnInit {
   productId!: number; 
   product: any; 
   quantity: number = 1;  
+  images: ProductImage [] = [];
+  activeImageIndex: number = 0;
 
 
   constructor(
     private route: ActivatedRoute, 
     private productService: ProductService,
-    private cartService: CartService  
+    private cartService: CartService,
+    private notify: NotificationService
   ) {}
 
   ngOnInit(): void {
@@ -32,24 +35,58 @@ export class ProductComponent implements OnInit {
   }
 
   loadProductData() {
-    
     this.productService.getSingleProduct(this.productId).subscribe({
       next: (data) => {
         this.product = data;
+       
+        this.productService.getProductImages(this.productId).subscribe({
+          next: (images) => {
+            this.images = images;
+            if (this.images.length > 0) {
+              this.activeImageIndex = 0; 
+            }
+          },
+          error: (err) => {
+            this.notify.showError('Error on Loading Images: ' + err);
+          },
+        });
       },
       error: (err) => {
-        console.error('Error on Loading Product:', err);
+        this.notify.showError('Error on Loading Product: ' + err);
       },
     });
   }
-
   
-  addToCart(): void {
+  addToCart() {
+    this.notify.showSuccess('Sucess  ! Product Add On Cart ')
     if (this.product) {
       this.cartService.addToCart( {
         ...this.product,
         quantity: this.quantity,
       })
+    }
+  }
+
+  setActiveImage(index: number): void {
+    this.activeImageIndex = index;
+  }
+
+  copyUrl () {
+
+    this.notify.showInfo('Copied url')
+
+    const url = window.location.href;
+    navigator.clipboard.writeText(url)
+  }
+
+  quantityItems: number = 1;
+
+
+  pickQuantity(incremento: number): void {
+    this.quantityItems += incremento;  
+
+    if (this.quantityItems < 1) {
+      this.quantityItems = 1
     }
   }
 
