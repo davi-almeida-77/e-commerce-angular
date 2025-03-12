f1fcdbf725758151f6e18944a9fbfebaeb26443a
DaviAlmeidaD3V <DaviAlmeidaDEV@hotmail.com>
2025-03-12 15:55:02 -0300

    [UPDATE] Improving CSS

--- client/src/app/components/order-history/order-history.component.ts
+++ client/src/app/components/order-history/order-history.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { OrderService } from '../../services/order.service';
 import { orderModel } from '../../shared/models/order.model';
 import { CommonModule } from '@angular/common';
-
+import { Subscription } from 'rxjs';
+import { NotificationService } from '../../services/notification.service';
 
 @Component({
   selector: 'app-order-history',
@@ -10,23 +11,101 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./order-history.component.css'],
   imports: [CommonModule]
 })
-export class OrderHistoryComponent implements OnInit {
+export class OrderHistoryComponent implements OnInit, OnDestroy {
   orders: orderModel[] = [];
+  private authSubscription!: Subscription;
+  userOn: boolean = false;
+  username: string | null = null;
+  groupedOrders: any[] = [];
+  orderVisibility: { [orderId: string]: boolean } = {}; 
 
-  constructor(private orderService: OrderService) { }
+  constructor( 
+    private orderService: OrderService,
+    private notify: NotificationService
+  ) { }
 
   ngOnInit(): void {
     this.loadOrders();
+    this.loadUser();
   }
 
   loadOrders() {
     this.orderService.getOrders().subscribe(
       (data) => {
         this.orders = data;
+        this.groupOrders();
       },
       (error) => {
-        console.error('Erro ao carregar pedidos:', error);
+        this.notify.showError('Error on loading orders');
       }
     );
+  }
+
+  loadUser(): void {
+    const authUserJson = sessionStorage.getItem('auth-user');
+    if (authUserJson) {
+      const authUser = JSON.parse(authUserJson);
+      this.username = authUser.username || null;
+    }
+  }
+
+  groupOrders(): void {
+
+    this.groupedOrders = [];
+  
+
+    this.orders.forEach(order => {
+
+      let existingOrder = this.groupedOrders.find(group => group.orderId === order.order_number);
+  
+      if (existingOrder) {
+
+        existingOrder.products.push({
+          product_name: order.product_name,
+          product_quantity: order.product_quantity,
+          product_price: this.parseToNumber(order.product_price), 
+          total_product_value: this.parseToNumber(order.total_product_value) 
+        });
+  
+
+        existingOrder.totalQuantity += order.product_quantity;
+        existingOrder.totalValue += this.parseToNumber(order.total_product_value);  
+      } else {
+
+        this.groupedOrders.push({
+          orderId: order.order_number,
+          products: [{
+            product_name: order.product_name,
+            product_quantity: order.product_quantity,
+            product_price: this.parseToNumber(order.product_price),  
+            total_product_value: this.parseToNumber(order.total_product_value)  
+          }],
+          totalQuantity: order.product_quantity,
+          totalValue: this.parseToNumber(order.total_product_value), 
+          orderDate: order.order_created_at
+        });
+      }
+  
+      
+      this.orderVisibility[order.order_number] = false;
+    });
+  }
+  
+
+  private parseToNumber(value: any): number {
+    return isNaN(parseFloat(value)) ? 0 : parseFloat(value); 
+  }
+  
+  
+
+  toggleOrderDetails(orderId: string): void {
+
+    this.orderVisibility[orderId] = !this.orderVisibility[orderId];
+  }
+
+  ngOnDestroy(): void {
+    if (this.authSubscription) {
+      this.authSubscription.unsubscribe();
+    }
   }
 }
