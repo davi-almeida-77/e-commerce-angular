3b22c6ebd6af9f64b48e993bf36b48b1826f7026
DaviAlmeidaD3V <DaviAlmeidaDEV@hotmail.com>
2025-03-13 11:54:53 -0300

    [ADD] Add Blog Route To Get Posts, For Blog Page

---
+++ backend/routes/blog.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../database/db'); 
+
+router.get("/", (req, res) => {
+    console.log(" Requisition For Posts  ");
+    
+    db.query("SELECT * FROM posts", (err, results) => {
+      if (err) {
+        console.log(err)
+      }
+      res.json(results);
+    });
+  });
+
+  router.get("/:id", (req, res) => {
+    const postId = req.params.id; 
+
+    console.log(`Request to obtain Post with ID: ${postId}`);
+
+    db.query("SELECT * FROM posts WHERE id = ?", [postId], (err, results) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ error: 'Error In Get Posts ' });
+        } else {
+            if (results.length > 0) {
+                res.json(results[0]);  
+            } else {
+                res.status(404).json({ error: 'Post Not Found ' });
+            }
+        }
+    });
+});
+
+
+module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,12 +3,14 @@ const router = express.Router();
 
 const usersRoute = require("./users");
 const productsRoute = require("./products");
-const authRoute =  require("./auth")
-const ordersRoute =  require("./orders")
+const authRoute =  require("./auth");
+const ordersRoute =  require("./orders");
+const blogRoute = require("./blog");
 
 router.use("/api/auth", authRoute)
 router.use("/api/users", usersRoute);  
 router.use("/api/products", productsRoute); 
 router.use("/api/orders", ordersRoute);
+router.use("/api/blog/posts", blogRoute)
 
 module.exports = router;
