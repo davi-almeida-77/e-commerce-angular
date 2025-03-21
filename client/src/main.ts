d62661e0ced2c09204025d79e97db0fa88c1bb9a
DaviAlmeidaD3V <DaviAlmeidaDEV@hotmail.com>
2025-03-21 10:57:29 -0300

    [ADD] Add Swiper Setup For Angular

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import { register as registerSwiperElement } from 'swiper/element/bundle';
 
+
+registerSwiperElement();
 platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true,
 })
