8faaaa021d3918ff6ea7e0f7acb5b0c1d84d0ebe
DaviAlmeidaD3V <DaviAlmeidaDEV@hotmail.com>
2025-03-27 16:27:21 -0300

    [UPDATE] Better Filters and Fix Bugs

--- client/src/app/components/shop/shop.component.ts
+++ client/src/app/components/shop/shop.component.ts
@@ -1,11 +1,9 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { CartService } from '../../services/cart.service';
 import { NotificationService } from '../../services/notification.service';
-
 import { productModel } from '../../shared/models/product.model'; 
 import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-shop',
@@ -13,73 +11,98 @@ import { Router } from '@angular/router';
   templateUrl: './shop.component.html',
   styleUrls: ['./shop.component.css'], 
 })
-export class ShopComponent implements OnInit {
+export class ShopComponent implements OnInit, OnChanges {
   products: productModel[] = []; 
+
 
   activeClass: boolean  = false;
 
   filterCategory: string = '';
   filterPrice: string = '';
-  filterModel: string = ''
+  filterModel: string = '';
 
-  itensByPage: number = 14;
-  currentLimit = this.itensByPage;
+  itensByPage: number = 0; 
+  currentLimit = this.itensByPage; 
+
+  limits: number[] = []; 
 
   constructor(
     private productService: ProductService, 
-    private router: Router ,
-    private cartService: CartService ,
+    private router: Router,
+    private cartService: CartService,
     private notify: NotificationService,
     private cdr: ChangeDetectorRef
-     ) {}  
+  ) {}  
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe(
       (data: productModel[]) => {  
-        this.products = data; 
+        this.products = data;
+        this.calcLimits();  
       },
       (error) => {
-        this.notify.showError('Error in Find Products ')
+        this.notify.showError('Error in Find Products');
       }
     );
 
     window.scrollTo(0, 0);
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['products']) {
+      this.calcLimits(); 
+    }
+  }
+
+
+  calcLimits(): void {
+    const totalProducts = this.products.length;
+
+
+    const limits = [
+      Math.floor(totalProducts / 1), 
+      Math.floor(totalProducts / 2), 
+      Math.floor(totalProducts / 3), 
+    ];
+
+    this.limits = limits.reverse();
+    this.currentLimit = limits[2]; 
+  }
+
+
   singleProductPage(productId: number): void {
     this.router.navigate([`/product/${productId}`]);
   }
 
+
   addToCart(product: productModel): void {
-    this.notify.showSuccess('Product added on Cart ')
+    this.notify.showSuccess('Product added on Cart');
     this.cartService.addToCart({
       ...product,
-      quantity: 1, 
+      quantity: 1,
     });
   }
 
+
   toggleActive(): void { 
-    this.activeClass =  !this.activeClass;
+    this.activeClass = !this.activeClass;
   }
 
-  changeLimit( newLimit:  number): void {
-    this.itensByPage = newLimit
+
+  changeLimit(newLimit: number): void {
+    this.itensByPage = newLimit;
     this.currentLimit = newLimit;
-
-
-    this.cdr.detectChanges();
+    this.cdr.detectChanges(); 
   }
 
-  resetFilters() {
+  resetFilters(): void {
     this.filterCategory = '';
     this.filterPrice = '';
     this.filterModel = '';
-    this.currentLimit =  14;
+
     this.changeLimit(this.currentLimit);
 
-    this.notify.showInfo('Reseted Filters ')
-
-    this.cdr.detectChanges();
+    this.notify.showInfo('Reseted Filters');
+    this.cdr.detectChanges(); 
   }
-  
 }
